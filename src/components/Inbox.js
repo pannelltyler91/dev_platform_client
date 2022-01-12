063c12b38297c574add79a871259a9a13a6506f3
Stella <stelladev7@gmail.com>
2022-01-11 21:16:16 -0500

    working more on inbox

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -1,10 +1,13 @@
 import {Component} from 'react';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import {NavLink} from 'react-router-dom';
 
 class Inbox extends Component {
     constructor() {
         super();
         this.state = {
-
+            chats: []
         }
     }
 
@@ -13,12 +16,23 @@ class Inbox extends Component {
         .then(res => res.json())
         .then(data => {
             console.log(data.chats);
+            this.setState({chats: data.chats}) 
         })
     }
     render() {
         return(
             <div>
-                <h1>Test Inbox</h1>
+                <h1>Inbox</h1>
+                <Container>
+                {this.state.chats.map((chat) => {
+                    return(
+                        
+                            <Card key={chat.id}>
+                               <NavLink to='/chat'><Card.Title>{chat.user2_name}</Card.Title></NavLink> 
+                            </Card>
+                    )
+                })}
+                </Container>
             </div>
         )
     }
